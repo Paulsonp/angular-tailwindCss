43a416290df0d13b3427e3923e443488b48cdee2
Paulsonps <paulsonps@neoito.com>
2019-10-30 18:02:13 +0530

    updation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  {
+    path: "landing",
+    loadChildren: () => import("./landing/landing.module").then(m => m.LandingModule)
+  },
+  {
+    path: "login",
+    loadChildren: () => import("./login/login.module").then(m => m.LoginModule)
+  },
+  { path: "**", redirectTo: "landing" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModulesComponent } from './modules.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ModulesComponent]
+})
+export class ModulesModule { }
