7970138453dd80f5dfe0d3a7780110115229ca6c
Paulsonps <paulsonps@neoito.com>
2019-11-05 10:07:36 +0530

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "", redirectTo: "/landing", pathMatch: "full" },
   {
     path: "landing",
     loadChildren: () => import("./landing/landing.module").then(m => m.LandingModule)
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LandingComponent } from './landing.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { LandingComponent } from "./landing.component";
+import { LandingRoutingModule } from "./landing-routing.module";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, LandingRoutingModule],
   declarations: [LandingComponent]
 })
-export class LandingModule { }
+export class LandingModule {}
